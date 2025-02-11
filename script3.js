045982e6250c45e0138b676cd92a724af02737c9
jhomml <jhomelherrera072006@gmail.com>
2025-02-12 00:30:08 +0100

    12/2/2025 Commit parte 3

---
+++ script3.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    setTimeout(function() {
+        let loadingScreen = document.getElementById('loading-screen');
+        let content = document.getElementById('content');
+
+        //  Aplicar la pantalla de carga
+        loadingScreen.style.opacity = '0';
+
+       // Transición
+        setTimeout(() => {
+            loadingScreen.style.display = 'none'; 
+            content.style.display = 'block'; 
+            setTimeout(() => {
+                content.style.opacity = '1';
+                content.style.transform = 'translateY(0)';
+            }, 50);
+        }, 1000); 
+    }, 500); // duracion de la pantalla de carga //
+};
