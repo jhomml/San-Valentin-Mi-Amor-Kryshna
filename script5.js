0a53a61c391fc19a4250623dc1b7e5926f2cfc32
jhomml <jhomelherrera072006@gmail.com>
2025-02-13 00:41:25 +0100

    Commit con la parte5

---
+++ script5.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+    setTimeout(function() {
+        let loadingScreen = document.getElementById('loading-screen');
+        let content = document.getElementById('content');
+
+        //  Aplicar la pantalla de carga
+        loadingScreen.style.opacity = '0';
+
+       // Transición
+        setTimeout(() => {
+            loadingScreen.style.display = 'none'; 
+            content.style.display = 'block'; 
+            setTimeout(() => {
+                content.style.opacity = '1';
+                content.style.transform = 'translateY(0)';
+            }, 50);
+        }, 1000); 
+    }, 500); // duracion de la pantalla de carga //
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("boton");
+    
+    button.addEventListener("click", function() {
+        if (button.click) {
+            setTimeout(() => {
+                window.location.href = "parte5.html";
+            }, 1000); 
+        }
+    });
+});
